cb0aaab5c547118de16853aaf767fe9251f4dd84
shishir bhandari <motivate23a@gmail.com>
2023-03-18 08:46:11 -0700

    Adding the adimation screen in the Splash Screen

--- lib/Pages/Splash/splashscreen.dart
+++ lib/Pages/Splash/splashscreen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/src/widgets/framework.dart';
-import 'package:flutter/src/widgets/placeholder.dart';
+import 'package:lottie/lottie.dart';
+
+import '../Login/login_page.dart';
 
 class SplashScreen extends StatefulWidget {
   const SplashScreen({super.key});
@@ -12,10 +13,41 @@ class SplashScreen extends StatefulWidget {
 class _SplashScreenState extends State<SplashScreen> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text("This is splash screen"),
-      ),
+    return FutureBuilder(
+      future: _getData(),
+      builder: (context, snapshot) {
+        if (snapshot.connectionState == ConnectionState.waiting) {
+          return Scaffold(
+            body: Column(
+              children: [
+                const SizedBox(
+                  height: 90,
+                ),
+                Center(
+                  child: Lottie.network(
+                    'https://assets5.lottiefiles.com/packages/lf20_bviewtx1.json',
+                  ),
+                ),
+                const Center(
+                  child: Text(
+                    "Car Parts Nepal",
+                    style: TextStyle(
+                        fontSize: 30,
+                        fontWeight: FontWeight.w900,
+                        color: Colors.red),
+                  ),
+                )
+              ],
+            ),
+          );
+        } else {
+          return const LoginScreen();
+        }
+      },
     );
+  }
+
+  Future<void> _getData() async {
+    await Future.delayed(const Duration(seconds: 3));
   }
 }
