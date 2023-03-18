f143c745390526b495364606efe8253271242f54
shishir bhandari <motivate23a@gmail.com>
2023-03-18 07:45:33 -0700

    Implementing Splash Screen

---
+++ lib/Pages/Splash/splashscreen.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/src/widgets/framework.dart';
+import 'package:flutter/src/widgets/placeholder.dart';
+
+class SplashScreen extends StatefulWidget {
+  const SplashScreen({super.key});
+
+  @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("This is splash screen"),
+      ),
+    );
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+import 'Constant/myapp.dart';
+
+void main() {
+  runApp(const MyApp());
+}
